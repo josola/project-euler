f78c6afbfd1790d072620d49d954ea7e38cd1453
josola <jordan.sola@gmail.com>
2020-09-09 16:23:39 -0500

    initial problem solve
    Initial draft for an algorithm to find the largest prime factor of any given number.

--- #3/src/main.c
+++ #3/src/main.c
@@ -4,8 +4,19 @@ euler problem #3
 Jordan Sola 2020
 */
 
+#include <stdio.h>
+
 int main()
 {
-    
+    long long unsigned int n = 600851475143;
+    long long unsigned int i = 2;
+    for (; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            n /= i;
+        }
+    }
+    printf("%llu\n", n);
     return 0;
 }
