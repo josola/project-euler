39dc1813a2099cfac20e5218dd05e304e895fadd
josola <jordan.sola@gmail.com>
2020-09-09 16:24:03 -0500

    solution draft
    Completed problem with initial draft of solution algorithm.

--- #5/src/main.c
+++ #5/src/main.c
@@ -4,7 +4,27 @@ project euler problem #5
 Jordan sola 2020
 */
 
+#include <stdio.h>
+
 int main()
 {
+    int minimum = 20;
+    int n = 20;
+
+    while (n > 0)
+    {
+        if ((minimum % n) == 0)
+        {
+            n--;
+        }
+        else
+        {
+            minimum++;
+            n = 20;
+        }
+    }
+
+    printf("%d%s", minimum, " is the smallest number that can be divided evenly from 1 to 20");
+
     return 0;
 }
