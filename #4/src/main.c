043021b5452b8a6155b172d8b342f01d10c03f43
josola <jordan.sola@gmail.com>
2020-09-09 16:23:57 -0500

    solution first draft
    Completed solution successfully, this is the first draft for the algorithm.

--- #4/src/main.c
+++ #4/src/main.c
@@ -32,6 +32,40 @@ bool CheckPalindrome(int original, int reversed)
 
 int main()
 {
+    int product = 0;
+    int reversed_number = 0;
+
+    bool found = false;
+    int x = 999;
+    int y = 999;
+    int minimum = 100;
+
+    while (!found)
+    {
+        product = x * y;
+        reversed_number = ReverseNumber(product);
+        
+        if (reversed_number == product)
+        {
+            found = true;
+            printf("%s%d\n", "Max palindrome = ", product);
+            printf("%s%d%s%d", "From: ", x, " * ", y);
+        }
+        else if (x == minimum && y == minimum)
+        {
+            printf("No palindrome found...");
+            break;
+        }
+        else if (y == minimum)
+        {
+            x--;
+            y = x - 1;
+        }
+        else
+        {
+            y--;
+        }
+    }
 
     return 0;
 }
