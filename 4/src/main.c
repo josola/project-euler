8ea09bee8aa4ba91294d2d7b3d5ff9ad92333e26
josola <jordan.sola@gmail.com>
2020-09-09 16:24:17 -0500

    algorithm re-design
    Rewrote original algorithm from scratch. Previous algorithm had multiple fatal flaws that produced incorrect answers. Current algorithm produces correct answer.

--- 4/src/main.c
+++ 4/src/main.c
@@ -5,67 +5,36 @@ Jordan Sola 2020
 */
 
 #include <stdio.h>
-#include <stdbool.h>
-
-int ReverseNumber(int number)
-{
-    int reversed_number = 0;
-    while (number > 0)
-    {
-        reversed_number = reversed_number * 10 + number % 10;
-        number = number / 10;
-    }
-    return reversed_number;
-}
-
-bool CheckPalindrome(int original, int reversed)
-{
-    if (original == reversed)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
 
 int main()
 {
-    int product = 0;
-    int reversed_number = 0;
+    int palindrome = 0;
 
-    bool found = false;
-    int x = 999;
-    int y = 999;
-    int minimum = 100;
-
-    while (!found)
+    for (int i = 100; i < 999; i++)
     {
-        product = x * y;
-        reversed_number = ReverseNumber(product);
-        
-        if (reversed_number == product)
+        for (int j = 100; j < 999; j++)
         {
-            found = true;
-            printf("%s%d\n", "Max palindrome = ", product);
-            printf("%s%d%s%d", "From: ", x, " * ", y);
-        }
-        else if (x == minimum && y == minimum)
-        {
-            printf("No palindrome found...");
-            break;
-        }
-        else if (y == minimum)
-        {
-            x--;
-            y = x - 1;
-        }
-        else
-        {
-            y--;
+            if ((i * j) % 10 != 0)
+            {
+                int product = i * j;
+                int temp_product = product;
+                int reverse = 0;
+
+                while (temp_product != 0)
+                {
+                    reverse = reverse * 10 + temp_product % 10;
+                    temp_product /= 10;
+                }
+
+                if ((reverse == product) && (reverse > palindrome))
+                {
+                    palindrome = product;
+                }
+            }
         }
     }
+
+    printf("%d\n", palindrome);
 
     return 0;
 }
