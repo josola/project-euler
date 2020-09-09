68448fd66c849f9eabbb1a3c2f25a013682594e3
josola <jordan.sola@gmail.com>
2020-09-09 16:23:08 -0500

    program #1 initial
    Initial source commit for solution to problem #1.

---
+++ #1/src/main.c
@@ -0,0 +1,18 @@
+/*
+main.c
+profect euler problem #1
+Jordan Sola 2020
+*/
+
+int main()
+{
+    int total = 8;
+    for (int i = 6; i < 1000; i++)
+    {
+        if ((i % 3 == 0) || (i % 5 == 0))
+        {
+            total += i;
+        }
+    }
+    return 0;
+}
