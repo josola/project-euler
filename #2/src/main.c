50181f9a9170ce63772db090e731995987f97a41
josola <jordan.sola@gmail.com>
2020-09-09 16:23:29 -0500

    first draft
    First draft of algorithm that solves the problem correctly.

--- #2/src/main.c
+++ #2/src/main.c
@@ -4,17 +4,24 @@ project euler problem #2
 Jordan Sola 2020
 */
 
+#include <stdio.h>
+
 int main()
 {
     unsigned long int next_term;
     unsigned long int term1 = 0;
     unsigned long int term2 = 1;
-    // value for total
-    // loop that begins at 0 and goes up to term limit
-        // next term = term1 + term2
-        // term1 = term2
-        // term2 = next term
-        // if (next term & 1) == 0
-            // total += next term
+    unsigned long int total = 0;
+    while (total < 4000000)
+    {
+        next_term = term1 + term2;
+        term1 = term2;
+        term2 = next_term;
+        if ((next_term & 1) == 0)
+        {
+            total += next_term;
+        }
+    }
+    printf("%lu\n", total);
     return 0;
 }
