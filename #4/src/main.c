8db581be5dd7f8bf84e88958020673d2a1caa285
josola <jordan.sola@gmail.com>
2020-09-09 16:23:55 -0500

    add supporting functions
    Added support functions to reverse the current product. Then check to see if the product is a palindrome against the original product.

--- #4/src/main.c
+++ #4/src/main.c
@@ -4,7 +4,34 @@ project euler #4
 Jordan Sola 2020
 */
 
+#include <stdio.h>
+#include <stdbool.h>
+
+int ReverseNumber(int number)
+{
+    int reversed_number = 0;
+    while (number > 0)
+    {
+        reversed_number = reversed_number * 10 + number % 10;
+        number = number / 10;
+    }
+    return reversed_number;
+}
+
+bool CheckPalindrome(int original, int reversed)
+{
+    if (original == reversed)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main()
 {
+
     return 0;
 }
