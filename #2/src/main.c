78c5dc9bfa1bc84dcda6dd050cb3d21bf15b9ef0
josola <jordan.sola@gmail.com>
2020-09-09 16:23:23 -0500

    pseudo code
    First draft of pseudo code for algorithm.

--- #2/src/main.c
+++ #2/src/main.c
@@ -6,5 +6,17 @@ Jordan Sola 2020
 
 int main()
 {
+    // value for next term
+    // value for term1 = 0
+    // value for term2 = 1
+    // value for total
+    // value for start iterator = 0;
+    // loop that begins at 0 and goes up to term limit
+        // next term = term1 + term2
+        // term1 = term2
+        // term2 = next term
+        // if current iterator = start iterator + 3
+            // total += next term
+            // start iterator = current iterator
     return 0;
 }
