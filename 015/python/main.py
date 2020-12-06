14af08a5576ddc85ad7600c76fb4566aeefbeb85
josola <jordan.sola@gmail.com>
2020-12-05 19:33:14 -0600

    Python Algorithm for Problem #15

---
+++ 015/python/main.py
@@ -0,0 +1,55 @@
+#
+# main.py
+# Project Euler Problem 15
+#
+# Jordan Sola 2020 - MIT License
+#
+# Starting in the top left corner of a 20x20 grid, and only
+# being able to move to the right and down, how many routes
+# are contained within the grid?
+
+# ----- global variables ----- #
+
+rows = []
+grid = []
+
+size = 21
+
+total_paths = 0
+
+# ----- build grid ----- #
+
+for i in range(size):
+
+    row = []
+
+    for j in range(size):
+
+        row.append(0)
+
+    grid.append(row)
+
+# ----- set ones in top row and first column ----- #
+
+for i in range(len(grid[0])):
+    grid[0][i] = 1
+
+for i in grid:
+    i[0] = 1
+
+# ----- calculate grid points ----- #
+
+for i in range(size):
+    for j in range(len(grid[i])):
+        if i == 0:
+            break
+        elif j == 0:
+            continue
+        else:
+            grid[i][j] = grid[i][j - 1] + grid[i - 1][j]
+
+total_paths += grid[size - 1][size - 1]
+
+# ----- done ----- #
+
+print(total_paths)
