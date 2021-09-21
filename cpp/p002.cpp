e1ba4482759ec6795b54092b049acca43b98f5e2
josola <jordan.sola@gmail.com>
2021-09-20 21:47:02 -0500

    Update Problem 2 (cpp)

--- cpp/p002.cpp
+++ cpp/p002.cpp
@@ -2,23 +2,37 @@
  * Project Euler
  * Problem 2 - Even Fibonacci numbers
  * (c) 2020-2021 Jordan Sola. All rights reserved. (MIT License)
+ * Written by Jordan Sola 2020-2021
  */
+
+#include <cmath>
 
 #include "stdio.h"
 
 int main() {
 
-	int a = 2;
-	int b = 8;
-	int sum = 0;
-
-	while (a < 4000000) {
-		sum += a;
-		int temp = b;
-		b = 4 * b + a;
-		a = temp;
-	}
+	const double phi = (1 + sqrt(5)) / 2;
+	const int limit = 4'000'000 / 3;
 	
+	/*
+	 * - nth fibonacci ferm formula: nth = [Phi^n – (phi)^n] / Sqrt[5]
+	 * - Every third term in a fibonacci sequence is even.
+	 */
+	
+	int sum = 0;
+	int term = 3;
+
+	while (sum < limit) {
+		sum += (pow(phi, term) - pow((1 - phi), term)) / sqrt(5);
+		term += 3;
+	}
+
 	printf("%i\n", sum);
+
 	return 0;
 }
+
+/*
+ * Execution time: 10µs (0.01ms)
+ * Complexity: O(log n)
+ */
