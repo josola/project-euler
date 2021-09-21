3d5f357dd1555787f8e520ab47213c6d7ce27ef4
josola <jordan.sola@gmail.com>
2021-09-21 14:07:42 -0500

    Update Runtime/Complexity

--- cpp/p003.cpp
+++ cpp/p003.cpp
@@ -10,11 +10,24 @@ int main() {
 
 	long long int num = 600851475143;
 
+	/*
+	 * Any prime number that is greater than a composite number cannot be the
+	 * largest prime, we elimenate primes greater than a constant by
+	 * dividing the num by its composite.
+	 */
+	
+
 	for (long int i = 2; i < num; i++) {
 		if (num % i == 0)
 			num /= i;
 	}
 
 	printf("%lli\n", num);
+	
 	return 0;
 }
+
+/*
+ * Runtime (Apple M1): 26µs (0.026ms)
+ * Complexity: O(log n)
+ */
