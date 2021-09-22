66ed5c74a9bf4d44a366585b9e1568960b83c102
josola <jordan.sola@gmail.com>
2021-09-22 11:08:55 -0500

    Update Problem 5 (cpp)

--- cpp/p005.cpp
+++ cpp/p005.cpp
@@ -4,32 +4,34 @@
  * (c) 2020-2021 Jordan Sola. All rights reserved. (MIT License)
  */
 
+#include <numeric>
 #include <iostream>
 
-using std::cout;
-using std::endl;
+#include "stdio.h"
 
-int main()
-{
-    // ----- global variables
-    int minimum = 20;
-    int n = 20;
-    // -----
+int main() {
+	
+	const int limit = 20;
+	
+	/*
+	 * - We're looking for the least common multiple (LCM) of the input set.
+	 * - We use a formula for the LCM by greatest common factor (GCF).
+	 * - Formula: LCM(a,b) = |(a×b)|/GCF(a,b)
+	 *   a = previous LCM in set, starting at the first term in the set
+	 *   b = term in set
+	 */
+	
+	long long int result = limit - (limit - 1);
 
-    while (n > 0)
-    {
-        if (minimum % n == 0)
-        {
-            n--;
-        }
-        else
-        {
-            minimum++;
-            n = 20;
-        }
-    }
+	for (int i = 2; i < limit + 1; i++)
+		result = abs(result * i) / std::gcd(result, i);
+	
+	std::cout << result << std::endl;
 
-   cout << minimum << " is the smallest number that can be divided evenly from 1 to 20" << endl;
-
-    return 0;
+	return 0;
 }
+
+/*
+ * Runtime (Apple M1): 250ns (0.25µs)
+ * Complexity: O(n)
+ */
