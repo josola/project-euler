2204ba2e2a7d3a81d05ff5d553ae46f6d1f2243e
josola <jordan.sola@gmail.com>
2021-09-25 22:16:44 -0500

    Update Problem 7 (cpp)

--- cpp/p007.cpp
+++ cpp/p007.cpp
@@ -4,50 +4,40 @@
  * (c) 2020-2021 Jordan Sola. All rights reserved. (MIT License)
  */
 
-#include <iostream>
+#include "stdio.h"
 
-using std::cout;
-using std::endl;
-
-int main()
-{
-    // ----- global veriables
-    int target_prime = 0;
-    int prime_count = 0;
-    // -----
-
-    while (prime_count != 10001)
-    {
-        // Current iteration.
-        target_prime++;
-
-        if (IsPrime(target_prime))
-        {
-            prime_count++;
-        }
-
-        cout << prime_count << ' ' << target_prime << endl;
-    }
-
-    return 0;
+int main() {
+	
+	const int limit = 10'001;
+	int count = 2;
+	int prime = 3;
+	
+	while (count < limit) {
+		
+		prime += 2;
+		bool is_prime = true;
+		
+		if (prime % 3 == 0)
+			continue;
+		
+		else {
+			
+			for (int i = 5; i * i <= prime; i += 6) {
+				if (prime % 3 == 0 || prime % i == 0 || prime % (i + 2) == 0)
+					is_prime = false;
+			}
+		}
+		
+		if (is_prime)
+			count++;
+	}
+	
+	printf("%i\n", prime);
+	
+	return 0;
 }
 
-int IsPrime(int n)
-{
-    // Can't be prime, no need to calculate.
-    if (n <= 1)
-    {
-        return 0;
-    }
-
-    // Positive divisors halt execution, number is not prime.
-    for (int i = 2; i < n; i++)
-    {
-        if (n % i == 0)
-        {
-            return 0;
-        }
-    }
-    
-    return 1;
-}
+/*
+ * Runtime (Apple M1): 2'379'208ns (2'379.21µs) (2.38ms)
+ * Complexity:
+ */
