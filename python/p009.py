499c366f72e62a9f03827771843604a7ffbe10f9
josola <jordan.sola@gmail.com>
2021-09-28 16:14:18 -0500

    Add Problem 9 (py)

---
+++ python/p009.py
@@ -0,0 +1,37 @@
+# Project Euler
+# Problem 9 - Special Pythagorean triplet
+# (c) Jordan Sola 2021. All rights reserved. (MIT License)
+# Written by Jordan Sola 2021
+
+import math
+
+def compute():
+
+	TARGET = 1000
+
+	a = 0
+	b = 0
+	c = 0
+
+	found = False
+	for a in range(1, TARGET):
+
+		for b in range(a, TARGET):
+
+			c = TARGET - a - b
+
+			first = math.pow(a, 2)
+			second = math.pow(b, 2)
+			third = math.pow(c, 2)
+
+			if first + second == third:
+				found = True
+				break
+
+		if found:
+			break
+
+	return (a * b * c)
+
+if __name__ == "__main__":
+	print(compute())
