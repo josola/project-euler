10a06ea69a55b271eb663054ef579ed31c0bee29
josola <jordan.sola@gmail.com>
2021-09-30 14:47:23 -0500

    Update Problem 12 (cpp)

--- cpp/p012.cpp
+++ cpp/p012.cpp
@@ -4,55 +4,54 @@
  * (c) 2020-2021 Jordan Sola. All rights reserved. (MIT License)
  */
 
-#include <iostream>
-#include <math.h>
+#include <cmath>
 
-using std::cout;
-using std::endl;
+#include "stdio.h"
 
-int main()
-{
-    // ----- global variables
-    int number = 1;
-    int i = 1;
-    // -----
-    
-    while (DivisorCount(number) < 500) {
-        number = TriangularGenerator(i);
-        i++;
-    }
-    
-    cout << number << endl;
-    
-    return 0;
+int main() {
+
+	int term = 1;
+	long int triangular = 0;
+	
+	int div_count = 0;
+	while (div_count < 500) {
+		
+		term++;
+		div_count = 0;
+		
+		/*
+		 * - Triangular numbers and arithmetic progressions can be found
+		 *   using the same algorithm.
+		 * - Triangular Number Algorithm: a = n * (n + 1) / 2
+		 *   Where "a" is the triangular number, and "n" is the last number
+		 *   in the arithmetic progression (term).
+		 * - See p001, and p006 for more examples of
+		 *   the arithmetic progression formula.
+		 */
+		
+		triangular = term * (term + 1) / 2;
+		
+		/*
+		 * - We can take the same algorithm problem 3 used to find
+		 *   the largest prme factor and adapt it to find all the
+		 *   factors belonging to our triangular number.
+		 */
+		
+		int root = sqrt(triangular);
+		for (int i = 1; i <= root + 1; i++) {
+			if (triangular % i == 0) {
+				div_count += 2;
+			}
+		}
+		
+	}
+	
+	printf("%li\n", triangular);
+	
+	return 0;
 }
 
-int DivisorCount(int number)
-{
-    int count = 0;
-    // Halves number of calculations needed.
-    int root = sqrt(number);
-
-    // Divisors divide the original number cleanly.
-    for (int i = 1; i <= root + 1; i++)
-    {
-        if (number % i == 0)
-        {
-            count += 2;
-        }
-    }
-
-    return count;
-}
-
-int TriangularGenerator(int position)
-{
-    int triangular_number = 0;
-
-    for (int i = 0; i < position; i++)
-    {
-        triangular_number += i;
-    }
-
-    return triangular_number;
-}
+/*
+ * Runttime (Apple M1):
+ * Complexity: O(log n)
+ */
