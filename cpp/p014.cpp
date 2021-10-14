e969252d1085d5b02ce4517a55729593b710b1ba
josola <jordan.sola@gmail.com>
2021-10-14 10:44:27 -0500

    Benchmark p014 (cpp)

--- cpp/p014.cpp
+++ cpp/p014.cpp
@@ -1,57 +1,68 @@
-/* 
+/*
  * Project Euler
+ * Problem 14 - Longest Collatz sequence
  * (c) 2020-2021 Jordan Sola. All rights reserved. (MIT License)
  */
 
 #include <iostream>
 
-using std::cout;
-using std::endl;
+#include "benchmark/benchmark.h"
 
-int main()
-{
-    // ----- global variables
-    long int start = 999999;
-    int highest_term = 0;
-    long int highest_start = 0;
-    // -----
+int compute(const int limit) {
 
-    while (start != 1)
-    {
-        // Start is its own iterator, need a seperate iterator.
-        long int current_term = start;
-        int terms = 0;
+	int max_start = 0;
+	int max_length = 0;
+	
+	/*
+	 * Every number in the bottom half of n has the same
+	 * reverse map in 2n, so we can start our sequence
+	 * at (limit / 2) + 1.
+	 */
 
-        // Perform Collatz algorithm.
-        while (current_term != 1)
-        {
-            current_term = Collatz(current_term);
-            terms++;
-        };
+	for (int i = (limit / 2) + 1; i < limit; i++) {
+		
+		long long int start = i;
+		int length = 0;
 
-        // Highest term and start are the end goal.
-        if (terms >= highest_term)
-        {
-            highest_term = terms;
-            highest_start = start;
-        }
+		while (start != 1) {
 
-        start--;
-    }
+			if (start % 2 == 0)
+				start /= 2;
+			else if (start % 2 != 0)
+				start = (3 * start) + 1;
 
-    cout << highest_term << endl
-         << highest_start << endl;
+			length++;
+
+		}
+		
+		if (length > max_length) {
+			max_length = length;
+			max_start = i;
+		}
+		
+	}
+
+	return max_start;
+
 }
 
-long int Collatz(long int n)
-{
-    if (n % 2 == 0)
-    {
-        n = n / 2;
-    }
-    else
-    {
-        n = (3 * n + 1)/2;
-    }
-    return n;
+static void p014_bench(benchmark::State& state) {
+	for (auto _ : state)
+		benchmark::DoNotOptimize(compute(state.range(0)));
 }
+
+BENCHMARK(p014_bench)->Arg(1'000'000)->Arg(2'000'000)->Arg(4'000'000)->Arg(8'000'000)->Unit(benchmark::kMillisecond);
+
+int main(int argc, char** argv) {
+	benchmark::Initialize(&argc, argv);
+	benchmark::RunSpecifiedBenchmarks();
+}
+
+// Answer: 837799
+
+// Run on (8 X 24.0449 MHz CPU s)
+// -----------------------------------------------------------------------
+// Benchmark                             Time             CPU   Iterations
+// -----------------------------------------------------------------------
+// p014_bench/1'000'000               92.6 ms         92.6 ms            8
+// p014_bench_BigO                       O(N)            O(N)
