65843fe6b3f9f04a9da0a11644540b7f9edbc3ee
josola <jordan.sola@gmail.com>
2021-10-15 12:07:45 -0500

    Benchmark P015 (cpp)

--- cpp/p015.cpp
+++ cpp/p015.cpp
@@ -5,44 +5,50 @@
  */
 
 #include <iostream>
+#include <cmath>
 
-using std::cout;
+#include "benchmark/benchmark.h"
 
-int main()
-{
-    // ----- global variables
-    const int width = 21;
-    const int height = 21;
-    // -----
+long int compute(const int width, const int height) {
+	
+	long int grid[height + 1][width + 1];
 
-    // Total number of paths is too long for standard int.
-    long int grid[width][height] = {};
+	/*
+	 * In a grid setting, each cell has as many
+	 * paths through it as the sum of the cell
+	 * directly above and the cell directly behind.
+	 */
 
-    // ----- Top row and first column elements have one path.
-    for (int i = 0; i < width; i++)
-    {
-        grid[0][i] = 1;
-    }
+	for (int i = 0; i < width + 1; i++)
+		grid[0][i] = 1;
+	for (int i = 0; i < height + 1; i++)
+		grid[i][0] = 1;
 
-    for (int i = 0; i < height; i++)
-    {
-        grid[i][0] = 1;
-    }
-    // -----
+	for (int i = 1; i < height + 1; i++)
+		for (int j = 1; j < width + 1; j++)
+			grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
 
-    // Each grid point gets its number of paths tracked.
-    for (int i = 1; i < height; i++)
-    {
-        for (int j = 1; j < width; j++)
-        {
-            grid[i][j] = grid[i - 1][j] + grid[i][j - 1];
-        }
-    }
-
-    // Bottom right corner of grid has the most paths going into it.
-    long int path_count = grid[width - 1][height - 1];
-
-    cout << path_count << '\n';
-
-    return 0;
+	return grid[height][width];
+	
 }
+
+static void p015_bench(benchmark::State& state) {
+	for (auto _ : state)
+		benchmark::DoNotOptimize(compute(20, 20));
+}
+
+BENCHMARK(p015_bench)->Repetitions(10);
+
+int main(int argc, char** argv) {
+	benchmark::Initialize(&argc, argv);
+	benchmark::RunSpecifiedBenchmarks();
+}
+
+// Answer: 137846528820
+
+// Run on (8 X 24.1203 MHz CPU s)
+// -----------------------------------------------------------------------
+// Benchmark                             Time             CPU   Iterations
+// -----------------------------------------------------------------------
+// p015_bench/20                      64.3 ns         64.3 ns      9681345
+// p015_bench_BigO                       O(N)            O(N)
