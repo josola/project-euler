41586bd70ffb55a27a688faaedee7181da624663
josola <jordan.sola@gmail.com>
2021-11-02 22:17:21 -0500

    Update Benchmark p019 (py)

--- python/p019.py
+++ python/p019.py
@@ -42,3 +42,4 @@ if __name__ == "__main__":
 # Asymptotic complexity: O(N)
 
 # M1           (3.2 GHz CPU) ARMv8-A64 (64 bit): 1000 loops, best of 5: 261 usec per loop
+# Ryzen 5 2600 (3.4 GHz CPU) x86-64 (64 bit): 500 loops, best of 5: 473 usec per loop
