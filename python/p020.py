27b27bc0bb5283d73d5731c772cb0bd27f64d0b7
josola <jordan.sola@gmail.com>
2021-11-10 22:42:01 -0600

    Ryzen Benchmark Scaffolding (py)

--- python/p020.py
+++ python/p020.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
 
 # Answer: 648
 
-# Asymptotic complexity: O(n)
+# Asymptotic complexity: O(N)
 
 # M1           (3.2 GHz CPU) ARMv8-A64 (64 bit): 20000 loops, best of 5: 13.4 usec per loop
+# Ryzen 5 2600 (3.4 GHz CPU) x86-64 (64 bit):
