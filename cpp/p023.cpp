883e3d2702f8483405d9870685542d544e11bbdf
josola <jordan.sola@gmail.com>
2022-01-14 19:11:09 -0600

    Initial Problem Solution (cpp)

---
+++ cpp/p023.cpp
@@ -0,0 +1,67 @@
+/* Project Euler
+ * Problem 23 - Non-abundant sums
+ * (c) 2020-2021 Jordan Sola. All rights reserved. (MIT License)
+ * Written by Jordan Sola 2021 */
+
+#include <vector>
+
+#include "benchmark/benchmark.h"
+
+int compute(const int n) {
+		
+	// Produce abundant numbers up to n
+	std::vector<int> abundant;
+	
+	for (int a = 1; a <= n; a++) {
+		int a_sum = 0;
+		for (int i = 1; i < a; i++) {
+			if (a % i == 0)
+				a_sum += i;
+		}
+		if (a_sum > a)
+			abundant.push_back(a);
+	}
+	
+	// Determine whether numbers up to n are the sum of two abundant numbers
+	std::vector<int> sum_abundant(n + 1, 0);
+	
+	for (int a = 0; a < abundant.size(); a++) {
+		for (int b = a; b < abundant.size(); b++) {
+			if (abundant[a] + abundant[b] <= n)
+				sum_abundant[abundant[a] + abundant[b]] = 1;
+			else
+				break;
+		}
+	}
+	
+	// Sum up numbers that are not a sum of two abundant numbers
+	int sum = 0;
+	for (int i = 1; i < n; i++) {
+		if (!sum_abundant[i])
+			sum += i;
+	}
+	
+	return sum;
+	
+}
+
+static void p023_bench(benchmark::State& state) {
+	for (auto _ : state)
+		compute(28123);
+}
+
+BENCHMARK(p023_bench)->Unit(benchmark::kMillisecond);
+
+int main(int argc, char** argv) {
+	benchmark::Initialize(&argc, argv);
+	benchmark::RunSpecifiedBenchmarks();
+}
+
+// Answer: 4'179'871
+
+/* Run on (8 X 24.1129 MHz CPU s) ARM64
+   -----------------------------------------------------
+   Benchmark           Time             CPU   Iterations
+   -----------------------------------------------------
+   p023_bench        254 ms          254 ms            2
+   p023_BigO              ?               ?              */
